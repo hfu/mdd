f16816d6e876b54ed4b5fc53fbc86e203e579c95
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-01-08 12:45:20 -0500

    update

--- mdd.rb
+++ mdd.rb
@@ -9,17 +9,17 @@ def mdd(dir, days)
     path = "#{dir}/#{day.iso8601.gsub('-', '/')}.md"
     raise "#{path} already exists!" if File.exist?(path)
     FileUtils.mkdir_p(File.dirname(path))
-    File.write(path, 
-	       "##{day.iso8601} #{WEEKDAYS[day.cwday]} W#{day.cweek} D#{day.yday}\n")
+    File.write(path,
+	       "# #{day.iso8601} #{WEEKDAYS[day.cwday]} W#{day.cweek} D#{day.yday}\n")
   end
 end
 
 if ARGV.size == 2
   mdd(ARGV[0], ARGV[1].to_i)
-else 
+else
   print <<-EOS
 ruby mdd.rb {directory} {days}
-   create empty markdown diary for specified days from today 
+   create empty markdown diary for specified days from today
    in the specified directory e.g., ruby mdd.rb . 100
   EOS
 end
